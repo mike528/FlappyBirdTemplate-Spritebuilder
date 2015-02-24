d60b9a5f5d53d94545cc7c6ab22c1b6f7967198f
Mike <mike@MMBP.local>
2015-02-24 16:38:32 +0800

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -4,16 +4,25 @@
 
 @implementation GamePlayScene
 
-- (void)initialize
+- (void) initialize
 {
     // your code here
+    character = (Character *) [CCBReader load:@"Character"];
+    [physicsNode addChild:character];
 }
 
--(void)update:(CCTime)delta
+-(void) update: (CCTime) delta
 {
     // put update code here
 }
 
 // put new methods here
+
+-(void) touchBegan: (CCTouch *) touch withEvent: (CCTouchEvent *) event {
+    
+    // this will get called every time the player touches the screen
+    [character flap];
+    
+}
 
 @end
