201bca366014628c87d7e25d8aa9cf7a8422d1d4
Mike <mike@MMBP.local>
2015-02-25 21:42:36 +0800

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -181,7 +181,7 @@
         }
     }
     
-    
+    /*
     // move and loop the bushes
     for (CCNode *bush in _bushes) {
         // move the bush
@@ -203,9 +203,8 @@
             cloud.position = ccp(cloud.position.x + 2 * cloud.contentSize.width, cloud.position.y);
         }
     }
+    */
     
-    
-    /*
     // loop the bushes
     for (CCNode *bush in _bushes) {
         // get the world position of the bush
@@ -240,7 +239,7 @@
             }
         }
     }
-    */
+    
     
     
     
